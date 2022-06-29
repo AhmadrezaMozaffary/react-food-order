d97a0c39b764142a05c51cdfa3207b18bbfe5722
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-06-29 22:01:05 +0430

    Add header component -- 1) Importing image using JS import ES6 syntax -- 2) Using CSS module to scope styling -- 3) HeaderCartButton re-usable component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import classes from "./Header.module.css";
+import mealsImg from "../../assets/meals.jpeg";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Food Order</h1>
+        <HeaderCartButton>Your Cart</HeaderCartButton>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="Delicious foods" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
